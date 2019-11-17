6573f0710c618c2b42686aaccdc8ca246a65146f
Tasztalos Pista <tasztalos69@gmail.com>
2019-11-17 23:14:44 +0100

    Add required&value checks

--- src/App.js
+++ src/App.js
@@ -64,238 +64,284 @@ export default class App extends React.Component {
 	};
 	async initGraph(e) {
 		e.preventDefault();
-
-		// Create main setup
-		const scene = new THREE.Scene();
-		const group = new THREE.Group();
-		scene.background = new THREE.Color(0xffffff);
-		const camera = new THREE.PerspectiveCamera(
-			75,
-			window.innerWidth / window.innerHeight,
-			0.1,
-			1000
-		);
-		const renderer = new THREE.WebGLRenderer({ antialias: true });
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		document.body.appendChild(renderer.domElement);
-		camera.position.z = 5;
-
-		//----------------------
-		// Create pyramid
-		//----------------------
-
-		// request configuration
-		let config = {
-				headers: { Accept: "text/json" }
-			},
-			statsF = [],
-			statsM = [];
-
-		// Generate arrays
-		for (let i = 0; i < 21; i++) {
-			statsM[i] = [];
-			statsF[i] = [];
-		}
-
-		// Create center separator
-		let lineZ =
-			(parseInt(this.state.yearEnd) - parseInt(this.state.yearStart)) * 0.05 +
-			0.05;
-		const centerLineGeometry = new THREE.BoxGeometry(0.03, 2.1, lineZ);
-		const materialBlack = new THREE.MeshBasicMaterial({ color: 0x101010 });
-		const centerLine = new THREE.Mesh(centerLineGeometry, materialBlack);
-		scene.add(centerLine);
-
-		// Year loop fn
-		const loopYears = (sex, observations, placement) => {
-			for (
-				let i = 0;
-				i < parseInt(this.state.yearEnd) - (parseInt(this.state.yearStart) - 1);
-				i++
+		if (
+			this.state.selectedCountry === null ||
+			this.state.yearStart === "" ||
+			this.state.yearEnd === ""
+		) {
+			alert("Fill in all");
+		} else {
+			let minYear = parseInt(this.state.years[0].name);
+			let maxYear = parseInt(
+				this.state.years[this.state.years.length - 1].name
+			);
+			if (
+				parseInt(this.state.yearStart) < minYear ||
+				parseInt(this.state.yearStart) > maxYear ||
+				parseInt(this.state.yearStart) < minYear ||
+				parseInt(this.state.yearStart) > maxYear
 			) {
-				let intake = observations[Object.keys(observations)[i]][0];
-				if (sex === "M") {
-					statsM[placement][i] = intake;
-				} else {
-					statsF[placement][i] = intake;
+				alert("Values!");
+			} else {
+				// Create main setup
+				const scene = new THREE.Scene();
+				const group = new THREE.Group();
+				while (scene.children.length > 0) {
+					scene.remove(scene.children[0]);
 				}
-			}
-		};
+				while (group.children.length > 0) {
+					group.remove(group.children[0]);
+				}
+				scene.background = new THREE.Color(0xffffff);
+				const camera = new THREE.PerspectiveCamera(
+					75,
+					window.innerWidth / window.innerHeight,
+					0.1,
+					1000
+				);
+				const renderer = new THREE.WebGLRenderer({ antialias: true });
+				renderer.setSize(window.innerWidth, window.innerHeight);
+				document.body.appendChild(renderer.domElement);
+				camera.position.z = 5;
 
-		// Main url
-		let mainUrl = `https://cors-anywhere.herokuapp.com/https://data.un.org/ws/rest/data/DF_UNData_WPP/SP_POP_TOTL.A.Y_LT5+Y5T10+Y10T14+Y15T19+Y20T24+Y25T29+Y30T34+Y35T39+Y40T44+Y45T49+Y50T54+Y55T59+Y60T64+Y65T69+Y70T74+Y75T79+Y80T84+Y85T89+Y90T94+Y95T99+Y_GE100.M._T.${parseInt(
-			this.state.selectedCountryId
-		)}.M?startPeriod=${this.state.yearStart}&endPeriod=${this.state.yearEnd}`;
+				//----------------------
+				// Create pyramid
+				//----------------------
 
-		await axios
-			.get(mainUrl, config)
-			.then((res) => {
-				let series = res.data.dataSets[0].series;
-				for (let age = 0; age < 21; age++) {
-					let observations = series[Object.keys(series)[age]].observations;
-					if (age === 0) {
-						loopYears("M", observations, 20);
-					} else if (age === 1) {
-						loopYears("M", observations, 0);
-					} else if (age === 12) {
-						loopYears("M", observations, 1);
-					} else if (age <= 11) {
-						loopYears("M", observations, age);
-					} else {
-						loopYears("M", observations, age - 1);
+				// request configuration
+				let config = {
+						headers: { Accept: "text/json" }
+					},
+					statsF = [],
+					statsM = [];
+
+				// Generate arrays
+				for (let i = 0; i < 21; i++) {
+					statsM[i] = [];
+					statsF[i] = [];
+				}
+
+				// Create center separator
+				let lineZ =
+					(parseInt(this.state.yearEnd) - parseInt(this.state.yearStart)) *
+						0.05 +
+					0.05;
+				const centerLineGeometry = new THREE.BoxGeometry(0.03, 2.1, lineZ);
+				const materialBlack = new THREE.MeshBasicMaterial({ color: 0x101010 });
+				const centerLine = new THREE.Mesh(centerLineGeometry, materialBlack);
+				scene.add(centerLine);
+
+				// Year loop fn
+				const loopYears = (sex, observations, placement) => {
+					for (
+						let i = 0;
+						i <
+						parseInt(this.state.yearEnd) - (parseInt(this.state.yearStart) - 1);
+						i++
+					) {
+						let intake = observations[Object.keys(observations)[i]][0];
+						if (sex === "M") {
+							statsM[placement][i] = intake;
+						} else {
+							statsF[placement][i] = intake;
+						}
+					}
+				};
+
+				// Main url
+				let mainUrl = `https://cors-anywhere.herokuapp.com/https://data.un.org/ws/rest/data/DF_UNData_WPP/SP_POP_TOTL.A.Y_LT5+Y5T10+Y10T14+Y15T19+Y20T24+Y25T29+Y30T34+Y35T39+Y40T44+Y45T49+Y50T54+Y55T59+Y60T64+Y65T69+Y70T74+Y75T79+Y80T84+Y85T89+Y90T94+Y95T99+Y_GE100.M._T.${parseInt(
+					this.state.selectedCountryId
+				)}.M?startPeriod=${this.state.yearStart}&endPeriod=${
+					this.state.yearEnd
+				}`;
+
+				await axios
+					.get(mainUrl, config)
+					.then((res) => {
+						let series = res.data.dataSets[0].series;
+						for (let age = 0; age < 21; age++) {
+							let observations = series[Object.keys(series)[age]].observations;
+							if (age === 0) {
+								loopYears("M", observations, 20);
+							} else if (age === 1) {
+								loopYears("M", observations, 0);
+							} else if (age === 12) {
+								loopYears("M", observations, 1);
+							} else if (age <= 11) {
+								loopYears("M", observations, age);
+							} else {
+								loopYears("M", observations, age - 1);
+							}
+						}
+					})
+					.catch((err) => console.log(err));
+
+				mainUrl = `https://cors-anywhere.herokuapp.com/https://data.un.org/ws/rest/data/DF_UNData_WPP/SP_POP_TOTL.A.Y_LT5+Y5T10+Y10T14+Y15T19+Y20T24+Y25T29+Y30T34+Y35T39+Y40T44+Y45T49+Y50T54+Y55T59+Y60T64+Y65T69+Y70T74+Y75T79+Y80T84+Y85T89+Y90T94+Y95T99+Y_GE100.F._T.${parseInt(
+					this.state.selectedCountryId
+				)}.M?startPeriod=${this.state.yearStart}&endPeriod=${
+					this.state.yearEnd
+				}`;
+				await axios.get(mainUrl, config).then((res) => {
+					let series = res.data.dataSets[0].series;
+					for (let age = 0; age < 21; age++) {
+						let observations = series[Object.keys(series)[age]].observations;
+						if (age === 0) {
+							loopYears("F", observations, 20);
+						} else if (age === 1) {
+							loopYears("F", observations, 0);
+						} else if (age === 12) {
+							loopYears("F", observations, 1);
+						} else if (age <= 11) {
+							loopYears("F", observations, age);
+						} else {
+							loopYears("F", observations, age - 1);
+						}
+					}
+				});
+
+				// Get the longest value
+				var maxRowM = statsM.map((row) => {
+					return Math.max.apply(Math, row);
+				});
+				var maxM = Math.max.apply(null, maxRowM);
+				var maxRowF = statsM.map((row) => {
+					return Math.max.apply(Math, row);
+				});
+				var maxF = Math.max.apply(null, maxRowF);
+				var max = Math.max(maxM, maxF);
+
+				const texShort = new THREE.MeshBasicMaterial({
+					map: new THREE.TextureLoader().load("texture_short.png")
+				});
+
+				const texLong = new THREE.MeshBasicMaterial({
+					map: new THREE.TextureLoader().load("texture_long.png")
+				});
+				const texMed = new THREE.MeshBasicMaterial({
+					map: new THREE.TextureLoader().load("texture_med.png")
+				});
+				const texMedLow = new THREE.MeshBasicMaterial({
+					map: new THREE.TextureLoader().load("texture_med_low.png")
+				});
+
+				// Display Males
+				for (let i = 0; i < statsM.length; i++) {
+					for (let j = 0; j < statsM[i].length; j++) {
+						let width = (statsM[i][j] / max) * 3;
+						let dashGeom = new THREE.BoxGeometry(width, 0.1, 0.05);
+						let dashMat;
+						if (width > 2) {
+							dashMat = [
+								texShort,
+								texShort,
+								texLong,
+								texLong,
+								texLong,
+								texLong
+							];
+						} else if (width > 1) {
+							dashMat = [texShort, texShort, texMed, texMed, texMed, texMed];
+						} else if (width > 0.1) {
+							dashMat = [
+								texShort,
+								texShort,
+								texMedLow,
+								texMedLow,
+								texMedLow,
+								texMedLow
+							];
+						} else {
+							dashMat = [
+								texShort,
+								texShort,
+								texShort,
+								texShort,
+								texShort,
+								texShort
+							];
+						}
+						let dash = new THREE.Mesh(dashGeom, dashMat);
+						group.add(dash);
+						dash.position.set(width / -2 - 0.016, -1 + i * 0.1, 0 + j * -0.05);
 					}
 				}
-			})
-			.catch((err) => console.log(err));
 
-		mainUrl = `https://cors-anywhere.herokuapp.com/https://data.un.org/ws/rest/data/DF_UNData_WPP/SP_POP_TOTL.A.Y_LT5+Y5T10+Y10T14+Y15T19+Y20T24+Y25T29+Y30T34+Y35T39+Y40T44+Y45T49+Y50T54+Y55T59+Y60T64+Y65T69+Y70T74+Y75T79+Y80T84+Y85T89+Y90T94+Y95T99+Y_GE100.F._T.${parseInt(
-			this.state.selectedCountryId
-		)}.M?startPeriod=${this.state.yearStart}&endPeriod=${this.state.yearEnd}`;
-		await axios.get(mainUrl, config).then((res) => {
-			let series = res.data.dataSets[0].series;
-			for (let age = 0; age < 21; age++) {
-				let observations = series[Object.keys(series)[age]].observations;
-				if (age === 0) {
-					loopYears("F", observations, 20);
-				} else if (age === 1) {
-					loopYears("F", observations, 0);
-				} else if (age === 12) {
-					loopYears("F", observations, 1);
-				} else if (age <= 11) {
-					loopYears("F", observations, age);
-				} else {
-					loopYears("F", observations, age - 1);
+				// Display Females
+				for (let i = 0; i < statsF.length; i++) {
+					for (let j = 0; j < statsF[i].length; j++) {
+						let width = (statsF[i][j] / max) * 3;
+						let dashGeom = new THREE.BoxGeometry(width, 0.1, 0.05);
+						let dashMat;
+						if (width > 2) {
+							dashMat = [
+								texShort,
+								texShort,
+								texLong,
+								texLong,
+								texLong,
+								texLong
+							];
+						} else if (width > 1) {
+							dashMat = [texShort, texShort, texMed, texMed, texMed, texMed];
+						} else if (width > 0.1) {
+							dashMat = [
+								texShort,
+								texShort,
+								texMedLow,
+								texMedLow,
+								texMedLow,
+								texMedLow
+							];
+						} else {
+							dashMat = [
+								texShort,
+								texShort,
+								texShort,
+								texShort,
+								texShort,
+								texShort
+							];
+						}
+						let dash = new THREE.Mesh(dashGeom, dashMat);
+						group.add(dash);
+						dash.position.set(width / 2 + 0.015, -1 + i * 0.1, 0 + j * -0.05);
+					}
 				}
-			}
-		});
+				var boundingBox = new THREE.Box3().setFromObject(group);
+				console.log();
 
-		// Get the longest value
-		var maxRowM = statsM.map((row) => {
-			return Math.max.apply(Math, row);
-		});
-		var maxM = Math.max.apply(null, maxRowM);
-		var maxRowF = statsM.map((row) => {
-			return Math.max.apply(Math, row);
-		});
-		var maxF = Math.max.apply(null, maxRowF);
-		var max = Math.max(maxM, maxF);
+				scene.add(group);
+				group.position.set(0, 0, boundingBox.getSize().z / 2 - 0.025);
 
-		const texShort = new THREE.MeshBasicMaterial({
-			map: new THREE.TextureLoader().load("texture_short.png")
-		});
+				// Add Orbit controller
+				const controls = new OrbitControls(camera, renderer.domElement);
+				controls.rotateSpeed = 0.5;
+				controls.zoomSpeed = 1.2;
+				controls.panSpeed = 0.8;
+				controls.maxDistance = 10;
+				controls.minDistance = 4;
+				controls.enableDamping = true;
 
-		const texLong = new THREE.MeshBasicMaterial({
-			map: new THREE.TextureLoader().load("texture_long.png")
-		});
-		const texMed = new THREE.MeshBasicMaterial({
-			map: new THREE.TextureLoader().load("texture_med.png")
-		});
-		const texMedLow = new THREE.MeshBasicMaterial({
-			map: new THREE.TextureLoader().load("texture_med_low.png")
-		});
+				// Render the scene
+				const animate = () => {
+					requestAnimationFrame(animate);
+					controls.update();
+					renderer.render(scene, camera);
+				};
+				animate();
 
-		// Display Males
-		for (let i = 0; i < statsM.length; i++) {
-			for (let j = 0; j < statsM[i].length; j++) {
-				let width = (statsM[i][j] / max) * 3;
-				let dashGeom = new THREE.BoxGeometry(width, 0.1, 0.05);
-				let dashMat;
-				if (width > 2) {
-					dashMat = [texShort, texShort, texLong, texLong, texLong, texLong];
-				} else if (width > 1) {
-					dashMat = [texShort, texShort, texMed, texMed, texMed, texMed];
-				} else if (width > 0.1) {
-					dashMat = [
-						texShort,
-						texShort,
-						texMedLow,
-						texMedLow,
-						texMedLow,
-						texMedLow
-					];
-				} else {
-					dashMat = [
-						texShort,
-						texShort,
-						texShort,
-						texShort,
-						texShort,
-						texShort
-					];
-				}
-				let dash = new THREE.Mesh(dashGeom, dashMat);
-				group.add(dash);
-				dash.position.set(width / -2 - 0.016, -1 + i * 0.1, 0 + j * -0.05);
+				// Handle resizing
+				window.addEventListener(
+					"resize",
+					() => {
+						camera.aspect = window.innerWidth / window.innerHeight;
+						camera.updateProjectionMatrix();
+						renderer.setSize(window.innerWidth, window.innerHeight);
+					},
+					false
+				);
 			}
 		}
-
-		// Display Females
-		for (let i = 0; i < statsF.length; i++) {
-			for (let j = 0; j < statsF[i].length; j++) {
-				let width = (statsF[i][j] / max) * 3;
-				let dashGeom = new THREE.BoxGeometry(width, 0.1, 0.05);
-				let dashMat;
-				if (width > 2) {
-					dashMat = [texShort, texShort, texLong, texLong, texLong, texLong];
-				} else if (width > 1) {
-					dashMat = [texShort, texShort, texMed, texMed, texMed, texMed];
-				} else if (width > 0.1) {
-					dashMat = [
-						texShort,
-						texShort,
-						texMedLow,
-						texMedLow,
-						texMedLow,
-						texMedLow
-					];
-				} else {
-					dashMat = [
-						texShort,
-						texShort,
-						texShort,
-						texShort,
-						texShort,
-						texShort
-					];
-				}
-				let dash = new THREE.Mesh(dashGeom, dashMat);
-				group.add(dash);
-				dash.position.set(width / 2 + 0.015, -1 + i * 0.1, 0 + j * -0.05);
-			}
-		}
-		var boundingBox = new THREE.Box3().setFromObject(group);
-		console.log();
-
-		scene.add(group);
-		group.position.set(0, 0, boundingBox.getSize().z / 2 - 0.025);
-
-		// Add Orbit controller
-		const controls = new OrbitControls(camera, renderer.domElement);
-		controls.rotateSpeed = 0.5;
-		controls.zoomSpeed = 1.2;
-		controls.panSpeed = 0.8;
-		controls.maxDistance = 10;
-		controls.minDistance = 4;
-		controls.enableDamping = true;
-
-		// Render the scene
-		const animate = () => {
-			requestAnimationFrame(animate);
-			controls.update();
-			renderer.render(scene, camera);
-		};
-		animate();
-
-		// Handle resizing
-		window.addEventListener(
-			"resize",
-			() => {
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-				renderer.setSize(window.innerWidth, window.innerHeight);
-			},
-			false
-		);
 	}
 	render() {
 		return (
@@ -357,11 +403,7 @@ export default class App extends React.Component {
 						min: {this.state.years[0].name} - max:{" "}
 						{this.state.years[this.state.years.length - 1].name}
 					</p>
-					<input
-						type='submit'
-						value='Submit'
-						onClick={this.initGraph.bind(this)}
-					/>
+					<button onClick={this.initGraph.bind(this)}>Submit</button>
 				</form>
 				<footer>
 					<p>
