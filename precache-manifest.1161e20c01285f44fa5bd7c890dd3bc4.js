16ec8001d1f80a22a5ec4007d36ff7483da43164
Tasztalos Pista <tasztalos69@gmail.com>
2019-11-22 00:09:13 +0100

    Updates

---
+++ precache-manifest.1161e20c01285f44fa5bd7c890dd3bc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8623108360dbb6799986f777c6dc73ca",
+    "url": "/age-pyramid/index.html"
+  },
+  {
+    "revision": "f6dfb360cb8ab5563743",
+    "url": "/age-pyramid/static/css/main.d6749af7.chunk.css"
+  },
+  {
+    "revision": "c47f2da097e836623d78",
+    "url": "/age-pyramid/static/js/2.25525ba6.chunk.js"
+  },
+  {
+    "revision": "f6dfb360cb8ab5563743",
+    "url": "/age-pyramid/static/js/main.d681a7ac.chunk.js"
+  },
+  {
+    "revision": "c370dc76c7f3b0ecbeaa",
+    "url": "/age-pyramid/static/js/runtime-main.4dcd2d19.js"
+  },
+  {
+    "revision": "bd4682b54c5028cf4c3a95c28751b645",
+    "url": "/age-pyramid/static/media/loading.bd4682b5.svg"
+  },
+  {
+    "revision": "8ce9b8e09e3f15d3828393e03ba5cf7b",
+    "url": "/age-pyramid/static/media/search.8ce9b8e0.svg"
+  }
+]);
